3991306fce3465b4b7fb031511c8ca8bf81328d9
Mauricio Miraglio <mauricio_miraglio@atlantico.com.br>
2020-02-17 10:22:24 -0300

    Added assertions to check functions are working ok

--- problem1.py
+++ problem1.py
@@ -3,6 +3,7 @@
 # For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 # Bonus: Can you do this in one pass?
 
+
 def my_add_up_to_number(data, k):
     """My solution."""
     for n1 in data:
@@ -12,16 +13,18 @@ def my_add_up_to_number(data, k):
                     "Found two numbers that add up to {}; ({} and {}).".format(k, n1, n2))
                 return True
 
+    print("No numbers add up to {}.".format(k))
     return False
 
 
 # Should return True
-print(my_add_up_to_number(data=[10, 15, 3, 7], k=17))
-print(my_add_up_to_number(data=[0, 15, 3, 0], k=0))
-print(my_add_up_to_number(data=[1, 100, -10, 7, 10], k=0))
-print(my_add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=95))
+assert my_add_up_to_number(data=[10, 15, 3, 7], k=17)
+assert my_add_up_to_number(data=[0, 15, 3, 0], k=0)
+assert my_add_up_to_number(data=[1, 100, -10, 7, 10], k=0)
+assert my_add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=95)
 # Should return False
-print(my_add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=1000))
+assert ~my_add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=1000)
+
 
 def so_add_up_to_number(data, k):
     """Stack Overflow accepted solution."""
@@ -40,14 +43,15 @@ def so_add_up_to_number(data, k):
                 "Found two numbers that add up to {}; ({} and {}).".format(k, data[i], data[j]))
             return True
 
+    print("No numbers add up to {}.".format(k))
     return False
 
 
 # Should return True
-print(so_add_up_to_number(data=[10, 15, 3, 7], k=17))
-print(so_add_up_to_number(data=[0, 15, 3, 0], k=0))
-print(so_add_up_to_number(data=[1, 100, -10, 7, 10], k=0))
-print(so_add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=95))
+assert so_add_up_to_number(data=[10, 15, 3, 7], k=17)
+assert so_add_up_to_number(data=[0, 15, 3, 0], k=0)
+assert so_add_up_to_number(data=[1, 100, -10, 7, 10], k=0)
+assert so_add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=95)
 
 # Should return False
-print(so_add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=1000))
+assert ~so_add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=1000)
