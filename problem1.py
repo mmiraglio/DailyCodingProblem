9b3aa7e4e686f6a0f035a2b95166b5eac2275bcf
Mauricio Miraglio <mauricio_miraglio@atlantico.com.br>
2020-02-17 10:11:08 -0300

    Added Stack Overflow solution

--- problem1.py
+++ problem1.py
@@ -3,9 +3,8 @@
 # For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 # Bonus: Can you do this in one pass?
 
-
-def add_up_to_number(data, k):
-
+def my_add_up_to_number(data, k):
+    """My solution."""
     for n1 in data:
         for n2 in data:
             if n1 + n2 == k:
@@ -16,10 +15,39 @@ def add_up_to_number(data, k):
     return False
 
 
-print(add_up_to_number(data=[10, 15, 3, 7], k=17))
-print(add_up_to_number(data=[0, 15, 3, 0], k=0))
-print(add_up_to_number(data=[1, 100, -10, 7, 10], k=0))
-print(add_up_to_number(data=[1, 100, -5, 7, 10, 94], k=95))
-print(add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=95))
+# Should return True
+print(my_add_up_to_number(data=[10, 15, 3, 7], k=17))
+print(my_add_up_to_number(data=[0, 15, 3, 0], k=0))
+print(my_add_up_to_number(data=[1, 100, -10, 7, 10], k=0))
+print(my_add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=95))
+# Should return False
+print(my_add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=1000))
 
-print(add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=1000))
+def so_add_up_to_number(data, k):
+    """Stack Overflow accepted solution."""
+    data = sorted(data)
+    i = 0
+    j = len(data) - 1
+
+    while i < j:
+        s = data[i] + data[j]
+        if s < k:
+            i = i + 1
+        elif s > k:
+            j = j - 1
+        else:
+            print(
+                "Found two numbers that add up to {}; ({} and {}).".format(k, data[i], data[j]))
+            return True
+
+    return False
+
+
+# Should return True
+print(so_add_up_to_number(data=[10, 15, 3, 7], k=17))
+print(so_add_up_to_number(data=[0, 15, 3, 0], k=0))
+print(so_add_up_to_number(data=[1, 100, -10, 7, 10], k=0))
+print(so_add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=95))
+
+# Should return False
+print(so_add_up_to_number(data=[100, 94, -5, 7, 10, 1], k=1000))
