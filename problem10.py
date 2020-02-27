1fa61b2ae1ca83ff18886b27ed7d66d3d7f9f722
Mauricio Miraglio <mauricio_miraglio@atlantico.com.br>
2020-02-27 15:25:23 -0300

    Thread based implementation for problem n10

--- problem10.py
+++ problem10.py
@@ -2,6 +2,8 @@
 # Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
 
 import datetime as dt
+import threading
+
 
 def set_interval(f, n):
     # Perform some basic validations
@@ -24,5 +26,18 @@ def custom_func():
     print("I was executed!")
 
 
+def custom_func2():
+    print("Yay... I was also executed!")
+
+
 if __name__ == "__main__":
-    set_interval(custom_func, 2500)
+    # Threads are set to daemon so are automatically terminated when all non-daemon threads (including main) exit.
+    # Thread 1
+    threading.Thread(target=set_interval, args=(
+        custom_func, 500), daemon=True).start()
+    # Thread 2
+    threading.Thread(target=set_interval, args=(
+        custom_func2, 1000), daemon=True).start()
+    while True:
+        if input('Press (q)uit to exit the program)') == "q":
+            break
