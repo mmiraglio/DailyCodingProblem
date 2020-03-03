8830338ab038bd308ee4efc88b7e1c59c6734e90
Mauricio Miraglio <mauricio_miraglio@atlantico.com.br>
2020-03-03 11:57:16 -0300

    Added a better solution for stock prices problem

--- stock_prices.py
+++ stock_prices.py
@@ -1,7 +1,7 @@
 import operator
 
 
-def get_max_profit(stock_prices):
+def my_get_max_profit(stock_prices):
     """ 
     Given a list of stock prices returns the maximum profit that could have been obtained by an investor 
     buying a single stock at the lowest price and selling at the max price.
@@ -57,24 +57,65 @@ def get_max_profit(stock_prices):
         return stock_prices_sorted[1][1] - stock_prices_sorted[0][1]
 
 
+def ic_get_max_profit(stock_prices):
+    """ 
+    This one's a good example of the greedy ↴ approach in action. 
+    Greedy approaches are great because they're fast (usually just one pass through the input). 
+    """
+    if len(stock_prices) < 2:
+        raise ValueError('Getting a profit requires at least 2 prices')
+
+    # We'll greedily update min_price and max_profit, so we initialize
+    # them to the first price and the first possible profit
+    min_price = stock_prices[0]
+    max_profit = stock_prices[1] - stock_prices[0]
+
+    # Start at the second (index 1) time
+    # We can't sell at the first time, since we must buy first,
+    # and we can't buy and sell at the same time!
+    # If we started at index 0, we'd try to buy *and* sell at time 0.
+    # This would give a profit of 0, which is a problem if our
+    # max_profit is supposed to be *negative*--we'd return 0.
+    for current_time in range(1, len(stock_prices)):
+        current_price = stock_prices[current_time]
+
+        # See what our profit would be if we bought at the
+        # min price and sold at the current price
+        potential_profit = current_price - min_price
+
+        # Update max_profit if we can do better
+        max_profit = max(max_profit, potential_profit)
+
+        # Update min_price so it's always
+        # the lowest price we've seen so far
+        min_price = min(min_price, current_price)
+
+    return max_profit
+
+
 if __name__ == "__main__":
     stock_prices = [10, 7, 5, 8, 11, 9]
     # Returns 6 (buying for $5 and selling for $11)
-    assert get_max_profit(stock_prices) == 6
+    assert my_get_max_profit(stock_prices) == 6
+    assert ic_get_max_profit(stock_prices) == 6
 
     stock_prices = [10, 10, 10]
     # Returns 0 because the stock price didn't changed during the day
-    assert get_max_profit(stock_prices) == 0
+    assert my_get_max_profit(stock_prices) == 0
+    assert ic_get_max_profit(stock_prices) == 0
 
     stock_prices = [10, 5, 10, 5, 10, 5]
     # Returns 5 (buying for $5 and selling for $10)
-    assert get_max_profit(stock_prices) == 5
+    assert my_get_max_profit(stock_prices) == 5
+    assert ic_get_max_profit(stock_prices) == 5
 
     stock_prices = [25, 30, 12, 20, 5, 12]
     # Returns 8 (buying for $12 and selling for $20)
-    assert get_max_profit(stock_prices) == 8
+    assert my_get_max_profit(stock_prices) == 8
+    assert ic_get_max_profit(stock_prices) == 8
 
     stock_prices = [50, 45, 40, 30, 20, 10]
     # Stocks just went down, so the best it can be done is
     # minimize the losses :(
-    assert get_max_profit(stock_prices) == -5
+    assert my_get_max_profit(stock_prices) == -5
+    assert ic_get_max_profit(stock_prices) == -5
